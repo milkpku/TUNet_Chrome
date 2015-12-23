9a178d0c15e6bdbdb625753fd28af1a0c5a6e6ec
milkpku <milkpku@gmail.com>
2015-12-24 00:42:44 +0800

    Load html

--- script/initial.js
+++ script/initial.js
@@ -1,10 +1,17 @@
 $(document).ready(function(){
     var conected = false;
     if (!conected){
-        $("#status").innerHTML="HELL";
         $("#status").load("login.html");
     }
     else{
-        $("#status").load("../html/succeed.html");
+        $("#status").load("succeed.html");
     }
 })
+
+function do_login (argument) {
+    $("#status").load("succeed.html");
+}
+
+function do_logout (argument) {
+    $("#status").load("login.html");
+}
